2709fbb5e9cbc78ad543facefca3b1b4c1d5fbf2
gustavoventieri <ventierigustavo@gmail.com>
2026-07-20 02:11:51 -0300

    fix(deps): java compile version fixed

--- framework/src/main/java/com/financial/api/framework/auth/adapter/out/persistence/EmailVerificationTokenAdapter.java
+++ framework/src/main/java/com/financial/api/framework/auth/adapter/out/persistence/EmailVerificationTokenAdapter.java
@@ -1,6 +1,4 @@
 package com.financial.api.framework.auth.adapter.out.persistence;
 
-import com.financial.api.auth.application.port.out.EmailVerificationTokenPort;
-
 public class EmailVerificationTokenAdapter implements EmailVerificationTokenPort {
 }
