a6bbc64ef51942545d6a7054a5f00685392b8c33
gustavoventieri <ventierigustavo@gmail.com>
2026-07-26 22:06:11 -0300

    fix(deploy): grype fixed, output format

--- framework/src/main/java/com/financial/api/framework/shared/token/RefreshTokenGenerator.java
+++ framework/src/main/java/com/financial/api/framework/shared/token/RefreshTokenGenerator.java
@@ -3,10 +3,22 @@ package com.financial.api.framework.shared.token;
 import com.financial.api.auth.application.port.in.refresh.GenerateRefreshTokenUseCase;
 import org.springframework.stereotype.Component;
 
+import java.security.SecureRandom;
+import java.util.Base64;
+
 @Component
 public class RefreshTokenGenerator implements GenerateRefreshTokenUseCase {
+
+    private static final SecureRandom SECURE_RANDOM = new SecureRandom();
+    private static final Base64.Encoder ENCODER = Base64.getUrlEncoder().withoutPadding();
+
     @Override
     public String execute() {
-        return "";
+
+        byte[] bytes = new byte[32];
+
+        SECURE_RANDOM.nextBytes(bytes);
+
+        return ENCODER.encodeToString(bytes);
     }
 }
